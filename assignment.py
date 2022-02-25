bd70ebf2d34624a9eb0479e539f3792c829e038b
Mateusz Slazynski <mateusz.slazynski@gmail.com>
2022-02-25 01:43:27 +0100

    The new assignment

---
+++ assignment.py
@@ -0,0 +1,50 @@
+from saport.simplex.model import Model
+
+#TODO:
+# Model assignments from assignment.pdf
+# tip 1. you may use an external solver to check if your models reach correct optima
+# tip 2. external solver available on-line: https://online-optimizer.appspot.com/?model=builtin:default.mod 
+
+def assignment_1():
+    model = Model("Assignment 1")
+
+    #TODO:
+    # Add:
+    # - variables
+    # - constraints
+    # - objective
+    # tip. value at optimum: 80.0
+    return model
+
+def assignment_2():
+    model = Model("Assignment 2")
+
+    #TODO:
+    # Add:
+    # - variables
+    # - constraints
+    # - objective
+    # tip. value at optimum: 10800.0
+    return model
+
+def assignment_3():
+    model = Model("Assignment 3")
+
+    #TODO:
+    # Add:
+    # - variables
+    # - constraints
+    # - objective
+    # tip. value at optimum: 0.0
+    return model
+
+def assignment_4():
+    model = Model("Assignment 4")
+
+    #TODO:
+    # Add:
+    # - variables
+    # - constraints
+    # - objective
+    # tip. value at optimum: 4000.0
+    return model

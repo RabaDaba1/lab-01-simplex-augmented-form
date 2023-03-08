e765ad773a67e6cdec0b033289e2da4fde99b44b
Kacper Rabczewski <krabczewski@gmail.com>
2023-03-08 08:18:03 +0000

    did assignment 1

--- assignment.py
+++ assignment.py
@@ -14,6 +14,17 @@ def assignment_1():
     # - constraints
     # - objective
     # tip. value at optimum: 80.0
+
+    x1 = model.create_variable("x1")
+    x2 = model.create_variable("x2")
+    x3 = model.create_variable("x3")
+
+    model.add_constraint(x1 + x2 + x3 <= 30)
+    model.add_constraint(x1 + 2 * x2 + x3 >= 10)
+    model.add_constraint(2 * x2 + x3)
+
+    model.maximize(2 * x1 + x2 + 3 * x3)
+
     return model
 
 def assignment_2():

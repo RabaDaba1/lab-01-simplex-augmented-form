c6acdf4c127cfaec15bdd739146170e16dd25f8a
Kacper Rabczewski <krabczewski@gmail.com>
2023-03-12 16:53:08 +0000

    Update file assignment.py

--- assignment.py
+++ assignment.py
@@ -21,9 +21,9 @@ def assignment_1():
 
     model.add_constraint(x1 + x2 + x3 <= 30)
     model.add_constraint(x1 + 2*x2 + x3 >= 10)
-    model.add_constraint(2 * x2 + x3)
+    model.add_constraint(2*x2 + x3 <= 20)
 
-    model.maximize(2 * x1 + x2 + 3 * x3)
+    model.maximize(2*x1 + x2 + 3*x3)
 
     return model
 
